dc64144350a47e62074b6794659177bf52fcb302
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-22 21:56:15 +0300

    save

---
+++ js/metanit.js
@@ -0,0 +1,100 @@
+// Условные операторы
+
+// "use strict";
+
+// const a = 1;
+// const b = 2;
+
+// const result = a < b ? a : b;
+// console.log(result);
+
+// "use strict";
+
+// const a = 1;
+// const b = 2;
+
+// const result = a < b ? a + b : a - b;
+
+// console.log(result);
+
+// const result = "hello " ?? "world";
+
+// console.log(result);
+
+// console.log(0 ?? 5);
+
+// console.log(false ?? true);
+
+// console.log("" ?? "javascript");
+
+// console.log(null ?? "not null");
+
+// console.log(null ?? null);
+
+// console.log(undefined ?? undefined);
+
+// const message = "Hello Javascript";
+// let text = null;
+// text ??= message;
+// console.log(text);
+
+// parseInt();
+// parseFloat();
+// isNaN();
+
+// const number1 = "56hek";
+// const test = "sssss";
+// console.log(parseInt(number1));
+// console.log(isNaN(number1));
+// console.log(isNaN(test));
+
+// Введение в массивы
+
+// const people = ["Tom", "Alice", "Sam"];
+// people[7] = "bob";
+
+// console.log(people[5]);
+// console.log(people[7]);
+// console.log(people[-2]);
+
+// const numbers1 = [0, 1, 2, 3, 4, 5];
+// const numbers2 = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+// ];
+
+// const people = [
+//   ["Tom", 25, false],
+//   ["Bill", 38, true],
+//   ["Alice", 21, false],
+// ];
+
+// console.log(people[0][2]);
+
+// const numbers = [];
+// numbers[0] = [];
+// numbers[0][0] = [];
+// numbers[0][0][0] = 5;
+// console.log(numbers[0][0][0]);
+
+// let income = 200;
+// switch (income) {
+//   case 100:
+//     console.log("Доход равен 100");
+//     income += 100;
+//     break;
+//   case 200:
+//     console.log("Доход равен 200");
+//     income += 100;
+//     break;
+//   case 500:
+//     console.log("Доход равен 500");
+//     income += 100;
+//     break;
+//     // default если значение не наидено
+//   default:
+//     console.log("Доход неизвестной величины");
+//     break;
+// }
+
+// console.log(`Финальный доход равен, ${income}`);
